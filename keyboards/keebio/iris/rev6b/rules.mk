c4fe3a5eab393983f4bea33aad34de819ea416e5
weberzach <weberzach@gmail.com>
2024-11-26 21:36:48 -0600

    Update rules.mk

--- keyboards/keebio/iris/rev6b/rules.mk
+++ keyboards/keebio/iris/rev6b/rules.mk
@@ -12,7 +12,7 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
 ENCODER_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 
 LTO_ENABLE = yes
